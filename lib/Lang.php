d54200f94a0b48f361b78c0f7b6f9d7d44564fbe
shabbyrobe <code@shabbyrobe.org>
2015-01-02 12:52:31 +1100

    added some documentation to interface

--- lib/Lang.php
+++ lib/Lang.php
@@ -10,7 +10,27 @@ namespace Tempe;
  */
 interface Lang
 {
+    /**
+     * @param object $handler
+     * @param object $node
+     * @param int $chainPos
+     * 
+     * @throws Tempe\Exception\Check  If the check fails
+     * @return bool  Must return true if the check passes
+     */
     function check($handler, $node, $chainPos);
-    function handle($handler, $in, HandlerContext $context);
-    function handleEmpty(HandlerContext $context);
+
+    /**
+     * @param object $handler
+     * @param mixed $in  Output of the previous handler in the 
+     *                   chain, or '' if the first.
+     */
+    function handle($handler, $in, \Tempe\HandlerContext $context);
+
+    /**
+     * Will be called if the handler is empty, i.e.:
+     *     {{ }}
+     *     {{#}}{{/}}
+     */
+    function handleEmpty(\Tempe\HandlerContext $context);
 }
